a9b27d9084e8585a4ef3a01f8ab51ff6bbff2e0f
OIITuMuCT <vataman@live.ru>
2025-09-02 22:05:26 +0300

    Creating a CRUD views URLs

--- tasks/mixins.py
+++ tasks/mixins.py
@@ -8,7 +8,7 @@ class SprintTaskMixin:
     Mixin to ensure a task  being created or updated is within
     the date range of its associated sprint.
     """
-    def dispatch(self, request, **args, **kwargs):
+    def dispatch(self, request, *args, **kwargs):
         task = self.get_object() if hasattr(self, "get_object") else None
         sprint_id = request.POST.get('sprint')
 
--- tasks/urls.py
+++ tasks/urls.py
@@ -1,8 +1,36 @@
-from django.urls import path
+from django.urls import path, register_converter
 from django.views.generic import TemplateView
 
+from . import views, converters
+
+from .views import (
+    TaskListView,
+    TaskDeleteView,
+    TaskCreateView,
+    TaskUpdateView,
+    TaskDetailView,
+)
+
 app_name = "tasks"  # This is for namespacing the URLs
+register_converter(converters.DateConverter, "yyyymmdd")
 urlpatterns = [
     path("", TemplateView.as_view(template_name="tasks/home.html"), name="home"),
     path("help/", TemplateView.as_view(template_name="tasks/help.html"), name="help"),
+    path("tasks/", TaskListView.as_view(), name="task-list"),  # GET
+    # path("tasks/<yyyymmdd:date>/", views.task_by_date()),
+    path("example/", views.example_view, name="example"),
+    path("tasks/new/", TaskCreateView.as_view(), name="task-create"),  # POST
+    path("tasks/home/", views.task_home, name="task-home"),
+    path("tasks/<int:pk>/", TaskDetailView.as_view(), name="task-detail"),
+    path(
+        "tasks/<int:pk>/edit/", TaskUpdateView.as_view(), name="task-update"
+    ),  # PUT/PATCH
+    path(
+        "tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"
+    ),  # DELETE
+    path(
+        "tasks/sprint/add_task/<int:pk>/",
+        views.create_task_on_sprint,
+        name="task-add-to-sprint",
+    ),
 ]

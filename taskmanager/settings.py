e7fdd67acdce554ced567b5d8ba2b21cf4afabf2
OIITuMuCT <vataman@live.ru>
2025-09-04 16:44:57 +0300

    Redis config

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -146,3 +146,13 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        },
+    }
+}

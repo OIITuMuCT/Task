78373f8c3830097bacfb2e6c23ad0f2d2c43d984
OIITuMuCT <vataman@live.ru>
2025-09-05 15:23:40 +0300

    Adding a URL password_reset

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,61 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
+from django.contrib.auth.views import (
+    LoginView,
+    LogoutView,
+    PasswordChangeView,
+    PasswordChangeDoneView,
+    PasswordResetView,
+    PasswordResetDoneView,
+    PasswordResetConfirmView,
+    PasswordResetCompleteView,
+)
 from . import views
+from tasks.views import TaskListView
 
 app_name = "accounts"
 
 urlpatterns = [
-    path("register/", views.register, name='register')
+    path("register/", views.register, name="register"),
+    path("profile/", TaskListView.as_view(), name="profile"),
+    path(
+        "login/", LoginView.as_view(template_name="accounts/login.html"), name="login"
+    ),
+    path("logout/", LogoutView.as_view(), name="logout"),
+    path(
+        "password_change/",
+        PasswordChangeView.as_view(
+            success_url=reverse_lazy("accounts:password_change_done"),
+            template_name="accounts/password_change_form.html",
+        ),
+        name="password_change",
+    ),
+    path(
+        "password_change/done/",
+        PasswordChangeDoneView.as_view(
+            template_name="accounts/password_change_done.html"
+        ),
+        name="password_change_done",
+    ),
+    path(
+        "password_reset/",
+        PasswordResetView.as_view(
+            email_template_name="accounts/custom_password_reset_email.html"
+        ),
+        name="password_reset",
+    ),
+    path(
+        "password_reset/done/",
+        PasswordResetDoneView.as_view(),
+        name="password_reset_done",
+    ),
+    path(
+        "password_reset/<uidb64>/<token>/",
+        PasswordResetConfirmView.as_view(),
+        name="password_reset_confirm",
+    ),
+    path(
+        "reset/done/",
+        PasswordResetCompleteView.as_view(),
+        name="password_reset_complete",
+    ),
 ]

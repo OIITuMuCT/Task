70203df50d0d253f548ecc714adbb9f8ee3ee333
OIITuMuCT <vataman@live.ru>
2025-09-08 23:01:37 +0300

    Initial commit: schemas.py

---
+++ tasks/schemas.py
@@ -0,0 +1,23 @@
+from ninja import Schema, ModelSchema, Field
+# from django.contrib.auth.models import User
+
+from .models import Task
+
+class TaskSchemaIn(ModelSchema):
+    title: str = Field(..., example="Enhanced Satellite Data Analisis")
+    description: str = Field(..., example="Develop a comprehensive analytical model to process")
+
+    class Config:
+        description = "Schema for creating a new task"
+        model = Task
+        model_fields = ["title", "description"]
+        model_fields_optional = ["status"]
+
+class TaskSchemaOut(ModelSchema):
+    # owner: UserSchema | None = Field(None)
+    class Config:
+        model = Task
+        model_fields = ['title', 'description']
+
+class CreateSchemaOut(Schema):
+    id: int = Field(..., example=1)

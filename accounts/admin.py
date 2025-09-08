305abab141cb39a83d77cb4899bf0d8ba41e4a92
OIITuMuCT <vataman@live.ru>
2025-09-08 20:07:42 +0300

    register Organization

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import UserProfile, Organization
+
+@admin.register(UserProfile)
+class AdminUserProfile(admin.ModelAdmin):
+    list_display = ('user', 'biography', 'photo')
+
+@admin.register(Organization)
+class AdminOrganization(admin.ModelAdmin):
+    list_display = ('name',)

96f0ecc40990af4e2f8056b8ce11f1a9542203ed
OIITuMuCT <vataman@live.ru>
2025-09-09 16:11:37 +0300

    Adding an archive tasks and TaskFilterSchema

--- tasks/api/tasks.py
+++ tasks/api/tasks.py
@@ -1,10 +1,16 @@
-from ninja import Router, Path
-from ninja.pagination import paginate
-from django.http import Http404
 from http import HTTPStatus
-from django.http import HttpRequest, HttpResponse
-
-from tasks.schemas import CreateSchemaOut, TaskSchemaIn, TaskSchemaOut, PathDate
+from django.http import HttpRequest, HttpResponse, Http404
+from ninja import Router, Path, Query
+from ninja.errors import HttpError
+from ninja.pagination import paginate
+from tasks.schemas import (
+    CreateSchemaOut,
+    TaskSchemaIn,
+    TaskSchemaOut,
+    PathDate,
+    TaskFilterSchema,
+)
+from tasks.enums import TaskStatus
 from tasks import services
 
 router = Router(tags=["tasks"])
@@ -18,8 +24,8 @@ def create_task(request: HttpRequest, task_in: TaskSchemaIn):
 
 @router.get("/", response=list[TaskSchemaOut])
 @paginate
-def list_tasks(request):
-    return services.list_tasks()
+def list_tasks(request: HttpRequest, filters: TaskFilterSchema = Query(...)):
+    return services.list_tasks(**filters.dict())
 
 
 @router.get("/{int:task_id}", response=TaskSchemaOut)
@@ -41,8 +47,15 @@ def delete_task(request: HttpRequest, task_id: int):
     services.delete_task(task_id=task_id)
     return HttpResponse(status=HTTPStatus.NO_CONTENT)
 
-@router.get('/archive/{year}/{month}/{day}', response=list[TaskSchemaOut])
+
+@router.get("/archive/{int:year}/{int:month}/{int:day}", response=list[TaskSchemaOut])
 @paginate
-def archive_tasks(request, created_at: PathDate = Path(...)):
-    return services.search_tasks(created_at=created_at.value(),
-        status=TaskStatus.ARCHIVED.value)
+def archived_tasks(request, created_at: PathDate = Path(...)):
+    return services.search_tasks(
+        created_at=created_at, status=TaskStatus.ARCHIVED.value
+    )
+
+@router.get("/error")
+def generate_error(request):
+    raise HttpError(status_code=400, detail="Custom error message")
+
--- tasks/schemas.py
+++ tasks/schemas.py
@@ -1,13 +1,16 @@
 import datetime
-from ninja import Schema, ModelSchema, Field
-
+from ninja import Schema, ModelSchema, Field, FilterSchema
+from pydantic import model_validator
 # from django.contrib.auth.models import User
-
+from tasks.enums import TaskStatus
 from .models import Task
+
 
 class TaskSchemaIn(ModelSchema):
     title: str = Field(..., example="Enhanced Satellite Data Analisis")
-    description: str = Field(..., example="Develop a comprehensive analytical model to process")
+    description: str = Field(
+        ..., example="Develop a comprehensive analytical model to process"
+    )
 
     class Config:
         description = "Schema for creating a new task"
@@ -15,20 +18,34 @@ class TaskSchemaIn(ModelSchema):
         model_fields = ["title", "description"]
         model_fields_optional = ["status"]
 
+
 class TaskSchemaOut(ModelSchema):
     # owner: UserSchema | None = Field(None)
     class Config:
         model = Task
-        model_fields = ['title', 'description']
+        model_fields = ["title", "description"]
+
+class TaskFilterSchema(FilterSchema):
+    title: str | None
+    status: TaskStatus | None
 
 class CreateSchemaOut(Schema):
     id: int = Field(..., example=1)
 
+
 class PathDate(Schema):
-    year: int
-    month: int
-    day: int
+    year: int = Field(..., ge=1)            # Year must be greater than or equal to 1.
+    month: int = Field(..., ge=1, le=12)    # Month must be between 1 and 12.
+    day: int = Field(..., ge=1, le=31)      # Day must be between 1 and 31.
+
+    @model_validator(mode='after')
+    def validate_date(self) -> PathDate:
+        try:
+            return datetime.date(self.year, self.month, self.day)
+        except ValueError:
+            raise ValueError(
+                f"The date {self.year}-{self.month}-{self.day} is not valid."
+            )
 
     def value(self):
         return datetime.date(self.year, self.month, self.day)
-

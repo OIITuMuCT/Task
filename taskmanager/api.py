46039404f4f1724c3508642b182fc96a9f0078c6
OIITuMuCT <vataman@live.ru>
2025-09-09 16:09:47 +0300

    Adding an exception_handler

--- taskmanager/api.py
+++ taskmanager/api.py
@@ -1,7 +1,15 @@
 from ninja import NinjaAPI
-
+from django.core.exceptions import ObjectDoesNotExist
 from tasks.api.tasks import router as tasks_router
 
 api = NinjaAPI()
 
 api.add_router('/tasks/', tasks_router)
+
+
+@api.exception_handler(ObjectDoesNotExist)
+def on_object_does_not_exist(request, exc):
+    """
+    Custom exception handler for ObjectDoesNotExist.
+    """
+    return api.create_response(request, {"message": "Object not found."}, status=404)
